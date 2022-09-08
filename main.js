dd40e633be66f099583afda66eab4be291cdc12d
Leology0 <leology42@gmail.com>
2022-09-08 09:13:04 -0600

    function clean up minecraft fishing

--- main.js
+++ main.js
@@ -26,36 +26,26 @@ function fishBtnClicked() {
   // Catch fish based on character
   if (character === 'steve') {
     // STEVE: Cod 70%, Salmon 20%, Tropical 5%, Puffer 5%
-    let randNum = Math.random();
-    if (randNum < 0.7) {
-      numCod++;
-      numCodSpan.innerHTML = numCod;
-      resultImg.src = 'img/Raw-Cod.png';
-    } else if (randNum < 0.9) {
-      numSalmon++;
-      numSalmonSpan.innerHTML = numSalmon;
-      resultImg.src = 'img/Raw-Salmon.png';
-    } else if (randNum < 0.95) {
-      numTropical++;
-      numTropicalSpan.innerHTML = numTropical;
-      resultImg.src = 'img/Tropical-Fish.png';
-    } else {
-      numPuffer++;
-      numPufferSpan.innerHTML = numPuffer;
-      resultImg.src = 'img/Pufferfish.png';
-    }
+    catchFish(0.7, 0.9, 0.95)
   } else if (character === 'alex') {
     // ALEX: Cod 10%, Salmon 10%, Tropical 30%, Puffer 50%
-    let randNum = Math.random();
-    if (randNum < 0.1) {
+    catchFish(0.1, 0.2, 0.5)
+  } else (character === 'villager');
+    catchFish(0.25, 0.5, 0.75)
+  
+}
+
+function catchFish(percent1, percent2, percent3) {
+  let randNum = Math.random();
+    if (randNum < percent1) {
       numCod++;
       numCodSpan.innerHTML = numCod;
       resultImg.src = 'img/Raw-Cod.png';
-    } else if (randNum < 0.2) {
+    } else if (randNum < percent2) {
       numSalmon++;
       numSalmonSpan.innerHTML = numSalmon;
       resultImg.src = 'img/Raw-Salmon.png';
-    } else if (randNum < 0.5) {
+    } else if (randNum < percent3) {
       numTropical++;
       numTropicalSpan.innerHTML = numTropical;
       resultImg.src = 'img/Tropical-Fish.png';
@@ -64,5 +54,4 @@ function fishBtnClicked() {
       numPufferSpan.innerHTML = numPuffer;
       resultImg.src = 'img/Pufferfish.png';
     }
-  }
 }
